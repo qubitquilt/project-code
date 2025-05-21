a5a6bff1e2680ec41d3b97fd0746b06b4827497c
GNUS <60074926+theMark001@users.noreply.github.com>
2025-05-21 19:12:12 +0200

    feat: add basic parsing system and simple args

---
+++ mycode.py
@@ -0,0 +1,59 @@
+#    File:         mycode.py                                                    ⣿⣿⣿⣿⣿⣿⣿⣿⣘⠿⠿⠿⢆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡸⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿    #
+#                                                                               ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣮⣭⣭⣴⣿⣿⣿⣿⣿⣿    #
+#    Project:      mycode                                                       ⣿⣿⣿⣿⠿⠿⠿⠿⠿⠏⠻⡿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⠿⠟⠹⠿⠿⠿⠿⠿⣿⣿⣿⣿⣿    #
+#    Github:       marsdevx                                                     ⣿⣿⣿⣇⠀⢀⣀⣀⢀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⠅⢀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠈⣿⣿⣿⣿    #
+#                                                                               ⣿⣿⣿⣿⣷⡘⣿⣿⡇⢀⣤⣤⣤⣀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⣀⣀⣐⡀⡀⣿⣿⣿⡏⣼⣿⣿⣿    #
+#    Created:      15:51   21/05/2025                                           ⣿⣿⣿⣿⣿⣿⣾⣿⣻⣝⣛⣛⣛⣫⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣛⠿⠿⠿⢟⣴⢿⣿⣴⣿⣿⣿⣿⣿    #
+#    Updated:      19:07   21/05/2025                                           ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿    #
+#                                                                               ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿    #
+#    Path:         ./mycode.py                                                  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿    #
+#                                                                               ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡛⠿⠟⣋⣍⣛⣋⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿    #
+
+import subprocess
+import argparse
+import parse
+
+def open_project(project_name, not_close):
+  all_projects = parse.collect_projects(parse.config["projects_dir"], parse.config["specific_projects"], parse.config["remove_rules"])
+  if project_name in all_projects:
+    project_path = all_projects[project_name]
+    print(f"Opening project '{project_name}'...")
+    subprocess.run(["code", project_path])
+    if not not_close:
+      print("Closing terminal...")
+      script = '''
+      tell application "iTerm2"
+          tell the current window
+              close
+          end tell
+      end tell
+      '''
+      subprocess.run(["osascript", "-e", script])
+  else:
+    print(f"Project '{project_name}' not found.")
+
+def main():
+  prog = argparse.ArgumentParser(prog="mycode")
+
+  prog.add_argument("-s", "--show", nargs="?", const="global", help="Show the data list")
+  prog.add_argument("project", nargs="?", help="Name of project to open")
+  prog.add_argument("-n", "--not-close", action="store_true", help="Don't close terminal after opening")
+
+  args = prog.parse_args()
+
+  if args.show:
+    if args.show == "global":
+      parse.print_proj()
+    elif args.show == "groups":
+      parse.print_groups()
+    elif args.show in parse.config["groups"]:
+      parse.print_group(args.show)
+    else:
+      print(f"Group '{args.show}' not found.")
+  elif args.project:
+    open_project(args.project, args.not_close)
+  else:
+    prog.print_help()
+
+if __name__ == "__main__":
+  main()
