408ef88a52d7e8309cae9a3fbc5b0b0d1a48a67c
GNUS <60074926+theMark001@users.noreply.github.com>
2025-05-22 13:39:52 +0200

    feat: add create project feature

---
+++ create_proj.py
@@ -0,0 +1,56 @@
+#                                                             ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿   #
+#    File:         create_proj.py                             ⡇⣶⠆⣴⣶⡶⢠⡖⡴⣴⠂⢄⣒⡂⡶⣐⣒⣒⣒⡂⢲⡆⢲⣦⠐⣶⣶⣶⣶⣶⠀⡔⣶⣶⣶⡶⢠⣶⡀⡆⣶⡶⠶⡀⢢⢶⡖⡀⠄⡲⣶⣶⣖⡆⠠⢰⣶⣆⢶⡄⢲⢸   #
+#                                                             ⡇⡟⡆⣿⣿⡇⣾⠃⣿⠃⢢⣿⣿⡇⣧⣿⣿⣿⣿⣷⡀⣿⢠⢻⣆⠜⣿⣿⣯⣿⡇⢧⢻⠛⣿⢃⣾⣿⡅⢧⣶⣿⣿⣿⣔⠕⠳⢳⡌⢄⠙⣿⣿⡼⢂⢃⢿⣿⡜⡇⣇⢸   #
+#    Project:      mycode                                     ⡇⢇⡇⣿⣿⠀⣿⠸⡟⢀⣿⣿⣿⣇⢸⣿⣿⣿⣿⣿⣧⡸⣇⣧⢿⣄⠸⣿⣿⣻⣿⢸⡜⡤⣿⢸⣿⣿⠇⣸⣿⣿⣿⣿⣿⣧⣻⡄⢿⣄⠄⠘⣿⣿⣼⡘⢸⣿⡇⡇⢸⢸   #
+#    Github:       marsdevx                                   ⡇⢸⡇⠟⣿⠀⡟⠀⢁⣾⣿⡿⢿⣛⡸⠿⠯⠿⣿⣛⡿⣷⣝⢞⢷⡙⢦⠈⢿⣿⣿⡀⣷⡄⠞⣾⣿⡿⣰⣿⣿⣿⡿⣿⣻⠿⠯⠿⠦⣝⢳⠄⠈⢿⡆⡇⢺⣿⡇⣇⢸⢸   #
+#                                                             ⡇⢸⡇⠠⣿⡀⡇⢀⣿⠿⠉⠈⠁⢀⣀⠀⠀⠀⠀⠀⠉⠓⢿⣯⣏⠻⣦⣕⠄⠙⢿⣧⠘⣿⣦⣻⣿⣿⣿⣿⣿⠗⠋⠁⠀⣀⠀⠀⠀⠀⠀⠉⠐⠀⠃⡃⢸⣿⡇⢸⢸⣸   #
+#    Created:      12:44   22/05/2025                         ⡇⡘⣿⠀⢺⡇⠃⢈⠁⠀⢠⣴⠀⣬⡍⠀⠀⠀⢠⣦⠀⣤⡐⢌⢿⣷⣬⣻⢷⣥⣀⠘⠳⡘⣿⣿⣿⣿⣿⠟⠅⣠⡔⢀⣬⡅⠀⠀⠀⣰⣦⠰⣤⠀⠀⠁⢸⡿⢀⡟⣿⢸   #
+#    Updated:      12:44   22/05/2025                         ⡇⡇⣿⡆⠈⢿⡀⢾⣿⣄⠸⣿⠸⣿⣧⠀⠀⢀⣾⣯⠶⢿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣶⣬⣌⣻⣿⣿⣿⣿⣴⣿⣧⢸⣿⣇⠀⠀⣠⣿⡷⠶⡟⢀⣾⠃⣾⠃⣼⠃⣿⢸   #
+#                                                             ⡇⣇⠿⣷⠐⡌⢧⠸⣿⣿⣷⢽⠣⠹⢿⣿⣿⡿⠿⢋⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡂⠹⠿⣿⣿⣿⠿⠣⣪⢵⣿⠇⣸⠃⢠⡟⣰⣿⢸   #
+#    Path:         ./create_proj.py                           ⡇⣿⢠⣿⡇⢹⡆⢣⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣴⠁⢰⢸⢱⢸⣿⢸   #
+#                                                             ⡇⣿⡼⢹⣿⡀⢻⣮⡂⠻⣿⣿⣿⡿⣽⣟⡾⣷⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣻⢯⡿⣽⣿⣿⠟⠑⢠⡆⠐⠇⠎⣾⣿⢸   #
+#                                                             ⣷⣶⣶⣶⣶⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣶⣶⣶⣾   #
+
+import os
+import requests
+import subprocess
+
+def create_github_repo(repo_name, username, token):
+    url = "https://api.github.com/user/repos"
+    headers = {"Authorization": f"token {token}"}
+    data = {"name": repo_name, "private": True}
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 201:
+        print(f"Successfully created private repository: {repo_name}")
+        return f"https://github.com/{username}/{repo_name}.git"
+    elif response.status_code == 422:
+        print(f"Repository {repo_name} already exists on GitHub.")
+        return f"https://github.com/{username}/{repo_name}.git"
+    else:
+        print(f"Error creating repository: {response.json()}")
+        return None
+
+def create_project(project_name, target_dir, username, token):
+    repo_url = create_github_repo(project_name, username, token)
+    if not repo_url:
+        print("Failed to create GitHub repository. Exiting.")
+        return
+
+    project_path = os.path.join(target_dir, project_name)
+
+    try:
+        subprocess.run(["git", "clone", repo_url, project_path], check=True)
+        os.chdir(project_path)
+        with open(".gitignore", "w") as gitignore:
+            gitignore.write("temp\nimprovements.md\n")
+        with open("README.md", "w") as readme:
+            readme.write("# Repository Status\n\nThis Git repository is currently in beta version. Features and functionality are subject to change. Feedback and contributions are welcome!\n")
+        subprocess.run(["git", "add", "."], check=True)
+        subprocess.run(["git", "commit", "-m", "chore: initial commit with README and .gitignore"], check=True)
+        subprocess.run(["git", "push"], check=True)
+        print(f"Project {project_name} has been created and initialized.")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+        return None
--- mycode.py
+++ mycode.py
@@ -9,9 +9,49 @@
 #    Path:         ./mycode.py                                                  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿    #
 #                                                                               ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡛⠿⠟⣋⣍⣛⣋⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿    #
 
+import os
+import json
 import parse
 import argparse
 import subprocess
+import create_proj as cp
+
+github_path = os.path.expanduser("~/.config/mycode/config.json")
+
+def check_json():
+  if not os.path.exists(github_path):
+    data = {
+      "github": {
+        "username": "",
+        "token": ""
+      }
+    }
+
+    with open(github_path, "w") as f:
+      json.dump(data, f, indent=2)
+
+def create(project_name, target_dir):
+  target_dir = os.path.expanduser(target_dir)
+  if not os.path.isdir(target_dir):
+      print(f"Target directory does not exist: {target_dir}")
+      return
+  
+  with open(github_path) as f:
+    config_file = json.load(f)
+  username = config_file["github"].get("username", "").strip()
+  token = config_file["github"].get("token", "").strip()
+
+  if not username.strip() or not token.strip():
+    username = input("Enter your GitHub username: ")
+    token = input("Enter your GitHub token: ")
+
+    config_file["github"]["username"] = username
+    config_file["github"]["token"] = token
+
+    with open(github_path, "w") as f:
+      json.dump(config_file, f, indent=2)
+
+  cp.create_project(project_name, target_dir, username, token)
 
 def open_project(project_name, not_close):
   all_projects = parse.collect_projects(parse.config["projects_dir"], parse.config["specific_projects"], parse.config["remove_rules"])
@@ -33,15 +73,23 @@ def open_project(project_name, not_close):
     print(f"Project '{project_name}' not found.")
 
 def main():
+  check_json()
   prog = argparse.ArgumentParser(prog="mycode")
 
   prog.add_argument("-s", "--show", nargs="?", const="global", help="Show the data list")
+  prog.add_argument("-c", "--create", nargs=2, metavar=("project_name", "target_dir"), help="Create a new project and repository")
   prog.add_argument("project", nargs="?", help="Name of project to open")
   prog.add_argument("-n", "--not-close", action="store_true", help="Don't close terminal after opening")
 
   args = prog.parse_args()
 
-  if args.show:
+  if args.create:
+    project_name, target_dir = args.create
+    create(project_name, target_dir)
+    all_projects = parse.collect_projects(parse.config["projects_dir"], parse.config["specific_projects"], parse.config["remove_rules"])
+    if project_name in all_projects:
+        open_project(project_name, False)
+  elif args.show:
     if args.show == "global":
       parse.print_proj()
     elif args.show == "groups":
